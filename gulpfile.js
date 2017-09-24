66efeb776bf315c5540efefecaefb18fe14e17ad
Jemima Luz <contatojemima@gmail.com>
2017-09-24 04:57:22 -0300

    coment linha 5

--- gulpfile.js
+++ gulpfile.js
@@ -2,5 +2,5 @@ var gulp = require('gulp'); //chama o gulp no projeto
 var sass = require('gulp-ruby-sass')//chama o sass no proj
 
 gulp.task('sass', function(){
-   return sass('sass/*.sass').pipe(gulp.dest('css'));
+   return sass('sass/*.sass').pipe(gulp.dest('css'));//compila em css os arquivos .sass 
 });
