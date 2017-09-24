8a08a4a6d12beae9ebd1433b5b56e0ff6de764e1
Jemima Luz <contatojemima@gmail.com>
2017-09-24 04:47:44 -0300

    adição do sass no projeto e criação da função sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp'); //chama o gulp no projeto
+var sass = require('gulp-ruby-sass')//chama o sass no proj
 
-gulp.task('default', function(){
-   console.log("coe rapaziada");
+gulp.task('sass', function(){
+   return sass('sass/*.sass').pipe(gulp.dest('css'));
 });
