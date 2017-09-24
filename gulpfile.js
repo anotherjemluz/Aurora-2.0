fdfe5939e02e2488dfd8fcdcd15f7d97ade9f355
Jemima Luz <contatojemima@gmail.com>
2017-09-24 20:47:56 -0300

    configuração plugin watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 var gulp = require('gulp'); //chama o gulp no projeto
 var sass = require('gulp-ruby-sass')//chama o sass no proj
+var watch = require('gulp-watch')//chama o watch no proj
 
 gulp.task('sass', function(){
-   return sass('sass/*.sass').pipe(gulp.dest('css'));//compila em css os arquivos .sass 
+   return sass('sass/**/*.sass').pipe(gulp.dest('css'));//compila em css os arquivos .sass 
 });
+
+gulp.task('watch', function(){
+   gulp.watch('sass/**/*.sass', ['sass']); 
+});
+
+gulp.task('default', ['sass', 'watch']);//executa as tasks acima automaticamente
